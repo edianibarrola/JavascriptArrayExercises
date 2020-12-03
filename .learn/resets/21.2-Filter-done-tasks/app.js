f0a4af255474f869bd59d839ddab17fa2ce237a6
edianibarrola <edianibarrola@gmail.com>
2020-12-03 17:00:38 +0000

    update

--- .learn/resets/21.2-Filter-done-tasks/app.js
+++ .learn/resets/21.2-Filter-done-tasks/app.js
@@ -10,11 +10,3 @@ let tasks = [
 ];
 
 //your code here
-// array.filter(function(currentValue, index, arr), thisValue)
-
-
-//variablename = (condition) ? value1:value2 
-
-//let bigCities = cities.filter(city => city.population > 3000000);
-let newArray = tasks.filter(tasks => tasks.done  );
-console.log(newArray);

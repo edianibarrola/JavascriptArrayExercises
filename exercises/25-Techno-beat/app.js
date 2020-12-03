4a2702593e37d9f785ce93ea3970a483b0921090
edianibarrola <edianibarrola@gmail.com>
2020-12-03 22:39:42 +0000

    update

--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,4 +1,32 @@
 
+const lyricsGenerator = (beatmap) => {
+    let threeInArowCtr = 0;
+    let lyr1 = 'Drop the base ';
+    let lyr0 = 'Boom ';
+    let lyr3InArow = '!!!Break the base!!! ';
+    let lyricsGenerated = [];
+    for (beat of beatmap){
+        if (beat == 1){
+            lyricsGenerated.push(lyr1);
+            threeInArowCtr++;
+            
+        }
+        if (beat == 0){
+            lyricsGenerated.push(lyr0);
+            threeInArowCtr=0;
+        }
+        if (threeInArowCtr == 3 ){
+            lyricsGenerated.push(lyr3InArow)
+        }
+    
+    
+    }return lyricsGenerated.join('')//return String(lyricsGenerated);
+}
+
+
+
+
+
 // test Data
 console.log(lyricsGenerator([0,0,1,1,0,0,0]))
 console.log(lyricsGenerator([0,0,1,1,1,0,0,0]))

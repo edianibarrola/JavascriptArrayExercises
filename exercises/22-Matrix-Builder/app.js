675a2ccda008b0a6cba508322d79f55cecd97d75
edianibarrola <edianibarrola@gmail.com>
2020-12-03 18:19:16 +0000

    update

--- exercises/22-Matrix-Builder/app.js
+++ exercises/22-Matrix-Builder/app.js
@@ -1,5 +1,18 @@
 // Code goes here
-
+let newArray=[];
+let paramString=[];
+const matrixBuilder = (param) => {
+    
+    for (let i=0; i< param; i++){
+         paramString.push(param);
+        
+    }
+    for (let i=0; i< param; i++){
+    newArray[i] = paramString;
+    
+    }
+    return newArray;
+}
 
 
 // do not change anything from this line down
